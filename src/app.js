01fa5204373c05a5cdbeb95250575a1fcd2cefb1
Jorge Alejandro Lavera <jlavera@gmail.com>
2023-02-14 09:45:10 -0300

    websockets base

--- src/app.js
+++ src/app.js
@@ -1,11 +1,17 @@
 import express from 'express';
+import handlebars from 'express-handlebars';
+import __dirname from './utils.js';
 import productRouter from './routes/product.router.js';
 import cartRouter from './routes/cart.router.js';
 
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+app.engine('handlebars',handlebars.engine());
+app.set('views',__dirname+'/views');
+app.set('view engine','handlebars');
 
+app.use(express.static(__dirname+'/public'));
 app.use('/api/products/',productRouter);
 app.use('/api/carts/',cartRouter);
 
